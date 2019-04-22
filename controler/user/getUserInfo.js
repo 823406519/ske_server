5dd12fca2a1b00d5c5c07ff84e104677d844f5a7
liuyaohui <qq823406519@126.com>
2019-04-22 08:42:28 +0800

    process: modify loginControl

--- controler/user/getUserInfo.js
+++ controler/user/getUserInfo.js
@@ -1,28 +1,28 @@
 const User = require('../../model/user')
 
-module.exports = async ctx =>{
-  
-  const {_id} = ctx.params
-  const user = await User.findOne({_id})
+module.exports = async ctx => {
+  const { _id } = ctx.params
+  const user = await User.findOne({ _id })
 
-  if(!user){
+  if (!user) {
     ctx.throw(500)
     return
   }
 
-
   ctx.body = {
-    status: user.status,
-    gender: user.gender,
-    age: user.age,
-    birthday: user.birthday,
-    location: user.location,
-    mtto: user.mtto,
-    introduction: user.introduction,
-    collections: user.collections,
-    resources: user.resources,
-    username: user.username,
-    email: user.email,
-    avatar: user.avatar
+    user: {
+      status: user.status,
+      gender: user.gender,
+      age: user.age,
+      birthday: user.birthday,
+      location: user.location,
+      mtto: user.mtto,
+      introduction: user.introduction,
+      collections: user.collections,
+      resources: user.resources,
+      username: user.username,
+      email: user.email,
+      avatar: user.avatar
+    }
   }
 }
--- middlewares/JWTErrorHandler.js
+++ middlewares/JWTErrorHandler.js
@@ -1,12 +1,13 @@
 module.exports = (ctx, next) => {
-  return next().catch((err) => {
-      if (401 == err.status) {
-          ctx.status = 401;
-          ctx.body = {
-              "message": "Not authorized"
-          };
-      } else {
-          throw err;
+  return next().catch(err => {
+    if (401 == err.status) {
+      ctx.status = 401
+      ctx.body = {
+        code: 0,
+        message: 'Not authorized'
       }
-  });
-};
+    } else {
+      throw err
+    }
+  })
+}
